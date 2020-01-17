5a5e39de38ec2d0b778dad579c850c6980f8bccb
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-01-17 15:01:31 -0300

    sidebar structure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <label></label>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
         </form>
       </aside>
       <main></main>
