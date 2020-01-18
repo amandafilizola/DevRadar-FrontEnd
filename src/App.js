2419a4a6946eda7269059d0eb12f485d445b55d7
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-01-18 15:54:20 -0300

    Creating CSS of main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import './global.css';
-import './App.css';
+import React from "react";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
@@ -10,29 +12,93 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do github</label>
-            <input name="github_username" id="github_username" required/>
+            <input name="github_username" id="github_username" required />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
+            <input name="techs" id="techs" required />
           </div>
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
+              <input name="latitude" id="latitude" required />
             </div>
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
+              <input name="longitude" id="longitude" required />
             </div>
           </div>
 
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main></main>
 
+      <main>
+        <ul>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
+                alt="foto"
+              />
+              <div>
+                <strong>Amanda Filizola</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
+            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
+                alt="foto"
+              />
+              <div className="user-info">
+                <strong>Amanda Filizola</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
+            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
+                alt="foto"
+              />
+              <div>
+                <strong>Amanda Filizola</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
+            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
+                alt="foto"
+              />
+              <div>
+                <strong>Amanda Filizola</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
+            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
+          </li>
+
+        </ul>
+      </main>
     </div>
   );
 }
