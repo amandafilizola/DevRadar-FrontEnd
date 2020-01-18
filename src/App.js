38f02b3a05323226569886978564772f0a838941
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-01-18 17:32:37 -0300

    finishing frontend

--- src/App.js
+++ src/App.js
@@ -1,102 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+    console.log(response.data);
+    setDevs([...devs, response.data]);
+  }
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
-                alt="foto"
-              />
-              <div>
-                <strong>Amanda Filizola</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
-            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
-                alt="foto"
-              />
-              <div className="user-info">
-                <strong>Amanda Filizola</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
-            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
-                alt="foto"
-              />
-              <div>
-                <strong>Amanda Filizola</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
-            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/17773694?s=460&v=4"
-                alt="foto"
-              />
-              <div>
-                <strong>Amanda Filizola</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>So far, a front-end developer in training. In love with React, and discovering Native</p>
-            <a href="https://github.com/amandafilizola">Acessar perfil no github</a>
-          </li>
-
+          {devs.map(dev => (
+            <DevItem dev={dev} key={dev._id} />
+          ))}
         </ul>
       </main>
     </div>
